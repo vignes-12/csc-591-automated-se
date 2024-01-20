c4dcbb23a43e29b7ae27e8e1e1ecf0a501022016
vignes-12 <obblivignes.kv@gmail.com>
2024-01-20 18:17:17 -0500

    [Implementation] Completed Task 1 Back-end

--- src/w3.py
+++ src/w3.py
@@ -1,51 +1,92 @@
 from config import parse_args, the
 from data import DATA
 import eg
+from sym import SYM
+from num import NUM
 
-weather = DATA("../data/weather.csv")
+def task_1(dataset):
+    for row in dataset.rows:
+            print(row.cells)
+    for col in weather.cols.x: #weatherClassCount += 1 #print("Column: ", weather.cols.x[col].txt)
+        # print(f"{weather.cols.x[col].txt}'s Numbers: {weather.cols.x[col].has}")
+        print(f"{weather.cols.x[col].txt:>28}")
+        print("============================")
 
-weatherClassCount = 0
-weatherRowCount = len(weather.rows)
+        for klassCategory in weather.cols.klass.has:
+            print(f"{klassCategory:>14}", end="")
+        print("\n")
+        
+        class_obj = weather.cols.x[col]
+        if isinstance(class_obj, SYM):
+            for classEntry in class_obj.has: # Can modify this to be dictionaries with values
+                classEntryIndexes = []
+                row_counter = 0
+                class_loc = class_obj.at
+                for row in dataset.rows:
+                     if row.cells[class_loc] == classEntry:
+                          classEntryIndexes.append(row_counter)
+                     row_counter += 1
+                classEntryDict = {}
+                for row_number in classEntryIndexes:
+                    row = dataset.rows[row_number]
+                    class_entry = row.cells[dataset.cols.klass.at]
+                    if class_entry not in classEntryDict:
+                         classEntryDict[class_entry] = 1
+                    else:
+                         classEntryDict[class_entry] += 1
+                print(classEntry, classEntryDict)
+        elif isinstance(class_obj, NUM):
+            class_loc = class_obj.at
+            class_obj_values = []
+            for row in dataset.rows:
+                 if row.cells[class_loc] not in class_obj_values:
+                      class_obj_values.append(row.cells[class_loc])
+            for classEntry in class_obj_values: # Can modify this to be dictionaries with values
+                classEntryIndexes = []
+                row_counter = 0
+                class_loc = class_obj.at
+                for row in dataset.rows:
+                     if row.cells[class_loc] == classEntry:
+                          classEntryIndexes.append(row_counter)
+                     row_counter += 1
+                classEntryDict = {}
+                for row_number in classEntryIndexes:
+                    row = dataset.rows[row_number]
+                    class_entry = row.cells[dataset.cols.klass.at]
+                    if class_entry not in classEntryDict:
+                         classEntryDict[class_entry] = 1
+                    else:
+                         classEntryDict[class_entry] += 1
+                print(classEntry, classEntryDict)
+        print("\n")
+    # for col in dataset.cols.x:
+    #     print(f"+---------------------------+")
+    #     print(f"|      Column: {dataset.cols.x[col].txt:<25}  |")
+    #     print(f"+--------------+------------+")
 
-for col in weather.cols.x: #weatherClassCount += 1 #print("Column: ", weather.cols.x[col].txt)
-    weatherClassCount += 1
-    # print(f"{weather.cols.x[col].txt}'s Numbers: {weather.cols.x[col].has}")
-    print(f"{weather.cols.x[col].txt:>28}")
-    print("============================")
-    for klassCategory in weather.cols.klass.has:
-        print(f"{klassCategory:>14}", end="")
-    print("\n")
+    #     col_values = dataset.cols.x[col]
+    #     if isinstance(col_values, SYM):
+    #         for col_val in col_values.has:
+    #             if '?' not in col_val:
+    #                 print(f"| {col_val:<25} |", end="")
+    #                 class_percentage = col_values.has[col_val] / len(dataset.rows) * 100
+    #                 print(f" {class_percentage:.2f}%    |")
+    #     elif isinstance(col_values, NUM):
+    #         pass
+    # print(f"+---------------------------+")
+    # print(f"|      Column: {dataset.cols.klass.txt:<25}  |")
+    # print(f"+--------------+------------+")
     
-    for classEntry in weather.cols.x[col].has: # Can modify this to be dictionaries with values
-        print(classEntry)
-    print("\n")
+    # col_values = dataset.cols.klass
+    # if isinstance(col_values, SYM):
+    #     for col_val in col_values.has:
+    #         print(f"| {col_val:<25} |", end="")
+    #         class_percentage = col_values.has[col_val] / len(dataset.rows) * 100
+    #         print(f" {class_percentage:.2f}%    |")
+    #     print(f"+--------------+------------+")
+    # elif isinstance(col_values, NUM):
+    #     pass
 
-# TODO:
-# Find a way to link the column names with the play! column (calculation) 
-# Overall just doing the math (we know how to pull the totals for class)
-# Need to figure out how to aggregate conditionals (windy == TRUE and play! == yes)
-print("\nWeather Klass: ", weather.cols.klass.has) 
-print("Weather Class Count: ", weatherClassCount)
-print("Weather Row Count: ", weatherRowCount)
-
-# for row in weather.rows:
-#     print(row.cells)
-
-# Find the klass --> data.cols.klass (play! in weather)
-# Access the possible options from klass ("yes" and "no" in weather) --> data.cols.klass.has
-    
-
-# diabetes = DATA("../data/diabetes.csv")
-# soybean = DATA("../data/soybean.csv")
-
-# diabetesClassCount = 0
-# diabetesRowCount = len(diabetes.rows)
-
-# for col in diabetes.cols.x: diabetesClassCount += 1
-
-# print("Diabetes Klass: ", diabetes.cols.klass.has)
-# print("Diabetes Class Count: ", diabetesClassCount)
-# print("Diabetes Row Count: ", diabetesRowCount)
-
-
+weather = DATA("../data/diabetes.csv")
+task_1(weather)
 
