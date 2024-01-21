9f4587c6bed154d1ae5fcf549f909e5da290b403
Zeiad Yakout <zeiad.yakout@gmail.com>
2024-01-20 19:49:44 -0500

    [Implementation] Created tables to report stats on data

--- src/w3.py
+++ src/w3.py
@@ -5,18 +5,18 @@ from sym import SYM
 from num import NUM
 
 def task_1(dataset):
-    for row in dataset.rows:
-            print(row.cells)
-    for col in weather.cols.x: #weatherClassCount += 1 #print("Column: ", weather.cols.x[col].txt)
+    # for row in dataset.rows:
+    #         print(row.cells)
+    for col in weather.cols.all: #weatherClassCount += 1 #print("Column: ", weather.cols.x[col].txt)
         # print(f"{weather.cols.x[col].txt}'s Numbers: {weather.cols.x[col].has}")
-        print(f"{weather.cols.x[col].txt:>28}")
+        print(f"{weather.cols.all[col].txt:>28}")
         print("============================")
 
         for klassCategory in weather.cols.klass.has:
             print(f"{klassCategory:>14}", end="")
         print("\n")
         
-        class_obj = weather.cols.x[col]
+        class_obj = weather.cols.all[col]
         if isinstance(class_obj, SYM):
             for classEntry in class_obj.has: # Can modify this to be dictionaries with values
                 classEntryIndexes = []
@@ -34,7 +34,10 @@ def task_1(dataset):
                          classEntryDict[class_entry] = 1
                     else:
                          classEntryDict[class_entry] += 1
-                print(classEntry, classEntryDict)
+                print(f"{classEntry:<11}", end="")
+                for val in classEntryDict.values():
+                    print(f"{val:>3}", end="           ")
+                print()
         elif isinstance(class_obj, NUM):
             class_loc = class_obj.at
             class_obj_values = []
@@ -57,36 +60,75 @@ def task_1(dataset):
                          classEntryDict[class_entry] = 1
                     else:
                          classEntryDict[class_entry] += 1
-                print(classEntry, classEntryDict)
-        print("\n")
-    # for col in dataset.cols.x:
-    #     print(f"+---------------------------+")
-    #     print(f"|      Column: {dataset.cols.x[col].txt:<25}  |")
-    #     print(f"+--------------+------------+")
+                print(f"{classEntry:<11}", end="")
+                for val in classEntryDict.values():
+                    print(f"{val:>3}", end="           ")
+                print()
+        print("           -----------------")
 
-    #     col_values = dataset.cols.x[col]
-    #     if isinstance(col_values, SYM):
-    #         for col_val in col_values.has:
-    #             if '?' not in col_val:
-    #                 print(f"| {col_val:<25} |", end="")
-    #                 class_percentage = col_values.has[col_val] / len(dataset.rows) * 100
-    #                 print(f" {class_percentage:.2f}%    |")
-    #     elif isinstance(col_values, NUM):
-    #         pass
-    # print(f"+---------------------------+")
-    # print(f"|      Column: {dataset.cols.klass.txt:<25}  |")
-    # print(f"+--------------+------------+")
-    
-    # col_values = dataset.cols.klass
-    # if isinstance(col_values, SYM):
-    #     for col_val in col_values.has:
-    #         print(f"| {col_val:<25} |", end="")
-    #         class_percentage = col_values.has[col_val] / len(dataset.rows) * 100
-    #         print(f" {class_percentage:.2f}%    |")
-    #     print(f"+--------------+------------+")
-    # elif isinstance(col_values, NUM):
-    #     pass
+        if isinstance(class_obj, SYM):
+            for classEntry in class_obj.has: # Can modify this to be dictionaries with values
+                classEntryIndexes = []
+                row_counter = 0
+                class_loc = class_obj.at
+                for row in dataset.rows:
+                     if row.cells[class_loc] == classEntry:
+                          classEntryIndexes.append(row_counter)
+                     row_counter += 1
+                classEntryDict = {}
+                for row_number in classEntryIndexes:
+                    row = dataset.rows[row_number]
+                    class_entry = row.cells[dataset.cols.klass.at]
+                    if class_entry not in classEntryDict:
+                        classEntryDict[class_entry] = 1
+                    else:
+                        classEntryDict[class_entry] += 1
+                print(f"{classEntry:<11}", end="")
+                for key, val in classEntryDict.items():
+                    if(weather.cols.all[col].txt == weather.cols.klass.txt):
+                        total = 0
+                        for num in weather.cols.klass.has.values():
+                            total += num
+                        print(f"{val}/{total:<3}", end="         ")
+                    else:
+                        print(f"{val}/{weather.cols.klass.has[key]:<3}", end="         ")
+                print()
+        elif isinstance(class_obj, NUM):
+            class_loc = class_obj.at
+            class_obj_values = []
+            for row in dataset.rows:
+                 if row.cells[class_loc] not in class_obj_values:
+                      class_obj_values.append(row.cells[class_loc])
+            for classEntry in class_obj_values: # Can modify this to be dictionaries with values
+                classEntryIndexes = []
+                row_counter = 0
+                class_loc = class_obj.at
+                for row in dataset.rows:
+                     if row.cells[class_loc] == classEntry:
+                          classEntryIndexes.append(row_counter)
+                     row_counter += 1
+                classEntryDict = {}
+                for row_number in classEntryIndexes:
+                    row = dataset.rows[row_number]
+                    class_entry = row.cells[dataset.cols.klass.at]
+                    if class_entry not in classEntryDict:
+                         classEntryDict[class_entry] = 1
+                    else:
+                         classEntryDict[class_entry] += 1
+                print(f"{classEntry:<11}", end="")
+                for key, val in classEntryDict.items():
+                    if(weather.cols.all[col].txt == weather.cols.klass.txt):
+                        total = 0
+                        for num in weather.cols.klass.has.values():
+                            total += num
+                        print(f"{val}/{total:<3}", end="         ")
+                    else:
+                        print(f"{val}/{weather.cols.klass.has[key]:<3}", end="         ")
+                print()
+        print()
 
-weather = DATA("../data/diabetes.csv")
+
+
+weather = DATA("../data/weather.csv")
 task_1(weather)
 
