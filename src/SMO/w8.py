8fcfc7b391e3b4f4a7fe376df18422c4ea3f9bfb
zhyakout <zhyakout@ncsu.edu>
2024-02-24 20:37:11 -0500

    [Implementation] Formatted prints and added smo9 prints

--- src/SMO/w8.py
+++ src/SMO/w8.py
@@ -11,21 +11,34 @@ def part1(filename, seed, repeats):
     print("seed :", seed)
     print("rows :", len(data.rows))
     print("cols :", len(data.cols.all))
-    #for i in range(0, len(data.rows)):
-    #    print(data.rows[i].cells)
-    #print(data.rows[i].cells for i in range(0, len(data.rows)))
 
-    print("{:<30} {:<50} {:>15}".format("names", "{:<3}".format(str(data.cols.names).replace(',', '    ')), "D2h-"))
-    # print("{:<30} {:<50} {:<7}".format("names", "['{}']".format("',   '".join(data.cols.names)), "-D2h"))
-    print("{:<30} {:<50} {:>15}".format("mid", str(data.mid().cells), str(data.mid().d2h(data)))) # TODO: FIGURE OUT ROUNDING PROBLEMS/D2H
-    print("{:<30} {:<50} {:>15}".format("div", str(data.div().cells), str(data.div().d2h(data)))) # TODO: FIGURE OUT ROUNDING PROBLEMS/D2H
-    print("#")
+    print("{:<30} {:<50} {:>23}".format("names", "{:<3}".format(str(data.cols.names).replace(',', '    ')), "D2h-"))
+
+    print(f"{'mid':<30} [", end="")
+    for cell in data.mid().cells:
+        print(f"{cell:<13.2f}", end="")
+    print(f"]     {data.mid().d2h(data):.2f}")
     
+    print(f"{'div':<30} [", end="")
+    for cell in data.div().cells:
+        print(f"{cell:<13.2f}", end="")
+    print(f"]     {data.div().d2h(data):.2f}")
+
+    print("#")
+
     for i in range(20):
-        best, rest = data.gate(budget0=9, budget=4, some=0.5)
-        print(best)
+        stats, bests = data.gate(budget0=9, budget=4, some=0.5)
+        print(f"{'smo9':<30} [", end="")
+        for cell in bests[0].cells:
+            print(f"{cell:<13}", end="")
+        print(f"]    ") # TODO: D2H VALUES NEED TO BE CALCULATED
         # print("{:<30} {:<50} {:>15}".format("smo9", result, result.d2h(result)))
 
+    print("#")
+
+    for i in range(20):
+        pass
+        # Need to implement the random 50
 
 filename = "../../data/auto93.csv"
 repeats = 20
