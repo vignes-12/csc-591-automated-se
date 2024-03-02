a763b628d0594913e41547d52238a32028d0bc77
raphijay <87781609+raphijay@users.noreply.github.com>
2024-03-02 02:53:26 -0500

    fixed issue with num that affected d2h calcs and more goodies

--- src/SMO/w8.py
+++ src/SMO/w8.py
@@ -4,6 +4,16 @@ from row import ROW
 import eg
 import random
 
+
+def std(data):
+    sum = 0
+    mid = data.mid().d2h(data)
+    for i in data.rows:
+        sum += (i.d2h(data) - mid) ** 2
+    return (sum / len(data.rows)) ** .5
+
+
+
 def part1(filename, seed, repeats):
     data = DATA(filename)
     # something ova here
@@ -24,7 +34,7 @@ def part1(filename, seed, repeats):
     print(f"{'div':<30} [", end="")
     for cell in data.div().cells:
         print(f"{cell:<13.2f}", end="")
-    print(f"]     {data.div().d2h(data):.2f}")
+    print(f"]     {std(data):.2f}")
 
     print("#")
 
@@ -56,7 +66,45 @@ def part1(filename, seed, repeats):
     print(f"]     {globalMin.d2h(data):.2f}")
 
 
+def part2(filename, seed, repeats):
+    data = DATA(filename)
+
+    print("date :", datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+    print("file :", filename)
+    print("repeats  :", repeats)
+    print("seed :", seed)
+    print("rows :", len(data.rows))
+    print("cols :", len(data.cols.all))
+    print("best :", round(min(data.rows, key=lambda row: row.d2h(data)).d2h(data), 2))
+    print("tiny: ", round(std(data) * 0.35,2))
+
+    print("#base", end =" ")
+    #base
+    print("#bonr9", end =" ")
+    #bonr9
+    print("#rand9", end =" ")
+    #rand9
+    print("#bonr15", end =" ")
+    #bonr15
+    print("#rand15", end =" ")
+    #rand15
+    print("#bonr20", end =" ")
+    #bonr20
+    print("#rand20", end =" ")
+    #rand20
+    print("#rand358", end =" ")
+    #rand358
+
+    # output stuff
+
+
+
+
+
+
+
 filename = "../../data/auto93.csv"
 repeats = 20
 seed = 31210
 part1(filename, seed, repeats)
+#part2(filename, seed, repeats)
